aff67cfdc2083bc1088a2697ec185fa45935f7e0
BuckanianMark <kariukimark115@gmail.com>
2024-08-23 23:45:43 +0300

    Removing data breadcrumbs

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -11,18 +11,14 @@ import { authGuard } from './core/guards/auth.guard';
 //import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 
 const routes: Routes = [
-  {path:"",component:HomeComponent,data:{breadcrumb:"Home"}},
-  {path:"test-error",component:TestErrorComponent,data:{breadcrumb:"Test Errors"}},
-  {path:"server-error",component:ServerErrorComponent,data:{breadcrumb:"Server Error"}},
-  {path:"not-found-error",component:NotFoundComponent,data:{breadcrumb:"Not Found"}},
-  {path:"shop",loadChildren:() => import("./shop/shop.module").then(mod => mod.ShopModule),
-    data:{breadcrumb:"Shop"}},
-  {path:"basket",loadChildren:() => import("./basket/basket.module").then(mod => mod.BasketModule),
-    data:{breadcrumb:"Basket"}},
-  {path:"checkout",loadChildren:() => import("./checkout/checkout.module").then(mod => mod.CheckoutModule),
-    data:{breadcrumb:"Checkout"},canActivate:[authGuard]},
-  {path:"account",loadChildren:() => import("./account/account.module").then(mod => mod.AccountModule),
-    data:{breadcrumb:{skip:true}}},
+  {path:"",component:HomeComponent},
+  {path:"test-error",component:TestErrorComponent},
+  {path:"server-error",component:ServerErrorComponent},
+  {path:"not-found-error",component:NotFoundComponent},
+  {path:"shop",loadChildren:() => import("./shop/shop.module").then(mod => mod.ShopModule)},
+  {path:"basket",loadChildren:() => import("./basket/basket.module").then(mod => mod.BasketModule)},
+  {path:"checkout",loadChildren:() => import("./checkout/checkout.module").then(mod => mod.CheckoutModule),canActivate:[authGuard]},
+  {path:"account",loadChildren:() => import("./account/account.module").then(mod => mod.AccountModule)},
   {path:"**",redirectTo:'not-found',pathMatch:'full'},
   
 ];
